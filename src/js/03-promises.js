521a56ba59802a9130d0f5bfe7da7ae1f2aff46c
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2023-08-11 10:15:05 +0300

    value check

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,39 @@
+// import notiflix from 'notiflix';
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve({ position, delay });
+//       } else {
+//         reject({ position, delay });
+//       }
+//     }, delay);
+//   });
+// }
+
+// document.querySelector('.form').addEventListener('submit', (event) => {
+//   event.preventDefault();
+
+//   const delay = parseInt(document.querySelector('[name="delay"]').value);
+//   const step = parseInt(document.querySelector('[name="step"]').value);
+//   const amount = parseInt(document.querySelector('[name="amount"]').value);
+
+//   for (let i = 0; i < amount; i++) {
+//     const position = i + 1;
+//     createPromise(position, delay + i * step)
+//       .then(({ position, delay }) => {
+//         notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       })
+//       .catch(({ position, delay }) => {
+//         notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//       });
+//   }
+// });
+
+
+
 import notiflix from 'notiflix';
 
 function createPromise(position, delay) {
@@ -20,6 +56,11 @@ document.querySelector('.form').addEventListener('submit', (event) => {
   const step = parseInt(document.querySelector('[name="step"]').value);
   const amount = parseInt(document.querySelector('[name="amount"]').value);
 
+  if (step < 0 || delay < 0 || amount <= 0) {
+    notiflix.Notify.failure('Please enter valid values for delay, step, and amount.');
+    return;
+  }
+
   for (let i = 0; i < amount; i++) {
     const position = i + 1;
     createPromise(position, delay + i * step)
@@ -31,3 +72,4 @@ document.querySelector('.form').addEventListener('submit', (event) => {
       });
   }
 });
+
